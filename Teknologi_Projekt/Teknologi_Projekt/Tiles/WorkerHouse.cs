1428fb678bc3be4398788cb5d1dbceb56ffc74d9
emje-dania <emje7@student.eadania.dk>
2025-02-10 22:47:49 +0100

    made the AddWorker method

--- Teknologi_Projekt/Teknologi_Projekt/Tiles/WorkerHouse.cs
+++ Teknologi_Projekt/Teknologi_Projekt/Tiles/WorkerHouse.cs
@@ -9,10 +9,18 @@ using System.Threading.Tasks;
 
 namespace Teknologi_Projekt.Tiles
 {
+   
+    /// <summary>
+    /// The WorkerHouse class adds more workers
+    /// for the player to assigne jobs in the game
+    /// </summary>
     internal class WorkerHouse : Tile
     {
+        private int workerCounter = 0;
+
         public WorkerHouse(Texture2D textureAtlas, int x, int y) : base(textureAtlas, x, y)
         {
+
         }
 
         public override void LoadContent(ContentManager content)
@@ -24,5 +32,11 @@ namespace Teknologi_Projekt.Tiles
         {
             throw new NotImplementedException();
         }
+
+        public void AddWorkers()
+        {
+            workerCounter += 2;
+        }
+
     }
 }
